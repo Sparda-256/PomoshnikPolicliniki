7cb791f783c95ac1f66372da64be661740cc4815
Антон Шмидт <sparda89328490316@mail.ru>
2024-11-30 11:19:14 +0300

    Добавьте файлы проекта.

---
+++ PomoshnikPolicliniki/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PomoshnikPolicliniki
+{
+    public class Patient : INotifyPropertyChanged
+    {
+        private int patientID;
+        private string fullName;
+        private int age;
+        private string gender;
+
+        public int PatientID
+        {
+            get => patientID;
+            set
+            {
+                patientID = value;
+                OnPropertyChanged(nameof(PatientID));
+            }
+        }
+        public string FullName
+        {
+            get => fullName;
+            set
+            {
+                fullName = value;
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+        public string Gender
+        {
+            get => gender;
+            set
+            {
+                gender = value;
+                OnPropertyChanged(nameof(Gender));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public class PatientSymptom
+    {
+        public string SymptomName { get; set; }
+    }
+
+    public class PatientDiagnosis
+    {
+        public string DiagnosisName { get; set; }
+        public int PercentageOfDiagnosis { get; set; }
+    }
+    public class PatientAnswer
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "data source=192.168.147.54;initial catalog=PomoshnikPolicliniki;user id=is;password=1;encrypt=False;";
+
+        public ObservableCollection<Patient> Patients { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Patients = new ObservableCollection<Patient>();
+            PatientsDataGrid.ItemsSource = Patients;
+            LoadPatients();
+        }
+
+        private void LoadPatients(string searchQuery = "")
+        {
+            Patients.Clear();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT PatientID, FullName, Age, Gender FROM Patients";
+
+                    if (!string.IsNullOrWhiteSpace(searchQuery))
+                    {
+                        query += " WHERE FullName LIKE @SearchQuery";
+                    }
+
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        if (!string.IsNullOrWhiteSpace(searchQuery))
+                        {
+                            cmd.Parameters.AddWithValue("@SearchQuery", "%" + searchQuery + "%");
+                        }
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Patients.Add(new Patient
+                                {
+                                    PatientID = reader.GetInt32(0),
+                                    FullName = reader.GetString(1),
+                                    Age = reader.GetInt32(2),
+                                    Gender = reader.GetString(3)
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке пациентов: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void SearchTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            string searchText = SearchTextBox.Text.Trim();
+            LoadPatients(searchText);
+        }
+        private void PatientsDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (PatientsDataGrid.SelectedItem is Patient selectedPatient)
+            {
+                PatientDetailWindow detailWindow = new PatientDetailWindow(selectedPatient.PatientID);
+                detailWindow.Owner = this;
+                detailWindow.ShowDialog();
+            }
+        }
+    }
+}
---
+++ PomoshnikPolicliniki/PatientDetailWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PomoshnikPolicliniki
+{
+    public partial class PatientDetailWindow : Window
+    {
+        private int patientID;
+        private string connectionString = "data source=192.168.147.54;initial catalog=PomoshnikPolicliniki;user id=is;password=1;encrypt=False;";
+
+        public Patient SelectedPatient { get; set; }
+        public ObservableCollection<PatientSymptom> PatientSymptoms { get; set; }
+        public ObservableCollection<PatientAnswer> PatientAnswers { get; set; }
+        public ObservableCollection<PatientDiagnosis> PatientDiagnoses { get; set; }
+
+        public PatientDetailWindow(int patientId)
+        {
+            InitializeComponent();
+            patientID = patientId;
+            PatientSymptoms = new ObservableCollection<PatientSymptom>();
+            PatientAnswers = new ObservableCollection<PatientAnswer>();
+            PatientDiagnoses = new ObservableCollection<PatientDiagnosis>();
+            DataContext = this;
+            LoadPatientDetailsAsync();
+        }
+
+        private async void LoadPatientDetailsAsync()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    await conn.OpenAsync();
+
+                    string patientQuery = "SELECT PatientID, FullName, Age, Gender FROM Patients WHERE PatientID = @PatientID";
+                    using (SqlCommand cmd = new SqlCommand(patientQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@PatientID", patientID);
+                        using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                SelectedPatient = new Patient
+                                {
+                                    PatientID = reader.GetInt32(0),
+                                    FullName = reader.GetString(1),
+                                    Age = reader.GetInt32(2),
+                                    Gender = reader.GetString(3)
+                                };
+                            }
+                        }
+                    }
+
+                    string symptomsQuery = @"
+                        SELECT S.SymptomName
+                        FROM PatientSymptoms PS
+                        JOIN Symptoms S ON PS.SymptomID = S.SymptomID
+                        WHERE PS.PatientID = @PatientID";
+                    using (SqlCommand cmd = new SqlCommand(symptomsQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@PatientID", patientID);
+                        using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                PatientSymptoms.Add(new PatientSymptom
+                                {
+                                    SymptomName = reader.GetString(0)
+                                });
+                            }
+                        }
+                    }
+
+                    string answersQuery = @"
+                        SELECT FQ.Question, A.Answer
+                        FROM PatientAnswers PA
+                        JOIN Answers A ON PA.AnswerID = A.AnswerID
+                        JOIN FollowUpQuestions FQ ON A.QuestionID = FQ.QuestionID
+                        WHERE PA.PatientID = @PatientID";
+                    using (SqlCommand cmd = new SqlCommand(answersQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@PatientID", patientID);
+                        using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                PatientAnswers.Add(new PatientAnswer
+                                {
+                                    Question = reader.GetString(0),
+                                    Answer = reader.GetString(1)
+                                });
+                            }
+                        }
+                    }
+
+                    string diagnosesQuery = @"
+                        SELECT D.DiagnosisName, PD.PercentageOfDiagnosis
+                        FROM PatientDiagnoses PD
+                        JOIN Diagnoses D ON PD.DiagnosisID = D.DiagnosisID
+                        WHERE PD.PatientID = @PatientID";
+                    using (SqlCommand cmd = new SqlCommand(diagnosesQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@PatientID", patientID);
+                        using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                PatientDiagnoses.Add(new PatientDiagnosis
+                                {
+                                    DiagnosisName = reader.GetString(0),
+                                    PercentageOfDiagnosis = reader.GetInt32(1)
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке деталей пациента: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
